7264b45d8cc18ec1e3aa26da97b13acac3b1a311
daipeng <daipeng@yuewen.com>
2019-07-15 18:49:45 +0800

    123

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Description:
+ * User: daipeng
+ * Date: 2019-07-15 18:38
+ * Project:common
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("i am common");
+
+    }
+}
